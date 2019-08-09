74b6f1bc186d9e3e9c8f341192153bcb5b4fd9d5
Agnes NM <agnesndutamwaniki@gmail.com>
2019-08-09 05:50:32 +0300

    add background image,and overlap it with row content, place the image behind the row content

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.getElementById('image1').style.display = "block";
+
+    // image is loaded at this time
+    // alert(`Image size: ${img.offsetWidth}x${img.offsetHeight}`);
+  };
