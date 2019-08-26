dfdaa622001d3445688e29179e58f32d4cd563d7
Agnes NM <agnesndutamwaniki@gmail.com>
2019-08-26 22:48:55 +0300

    fix conflicts

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    document.getElementById('image1').style.display = "block";
-
-    // image is loaded at this time
-    // alert(`Image size: ${img.offsetWidth}x${img.offsetHeight}`);
-  };
